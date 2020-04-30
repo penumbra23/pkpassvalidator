228d5185fc47b35942cc4836cdd105e9b02bbb99
Branimir Malesevic <Branimir.Malesevic@bam.fan>
2020-04-30 13:45:06 +0200

    Add healthcheck

---
+++ PassValidator.API/Controllers/HealthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PassValidator.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HealthController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Health()
+        {
+            return Ok("{\"health\":\"ok\"}");
+        }
+    }
+}
